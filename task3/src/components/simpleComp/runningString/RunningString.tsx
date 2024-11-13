ddbf96276f6bebabd09c7b0a786966e3e7e03156
Lenshmidt Andrei <lenshmidt.andrei@gmail.com>
2024-11-13 23:50:15 +0300

    fix: Исправлены стили

--- task3/src/components/simpleComp/runningString/RunningString.tsx
+++ task3/src/components/simpleComp/runningString/RunningString.tsx
@@ -3,21 +3,14 @@ import "./running.scss";
 
 export default function RunningString(prop: { text: string; color: string }) {
   const fill = prop.text === "descuento" ? "#fcfaf9" : " #1b1f21";
-<<<<<<< HEAD
   const arr = prop.text === "descuento" ? "12345678" : "12345678901234567";
-=======
->>>>>>> bb3a47431af344a21f53bb84486ae2fb4bb4dfc1
   return (
     <div
       className={`${prop.text}__runnig-string`}
       style={{ backgroundColor: prop.color }}
     >
       <div className="runnig-string__box">
-<<<<<<< HEAD
         {arr.split("").map((_, index) => (
-=======
-        {"123456789012345678".split("").map((_, index) => (
->>>>>>> bb3a47431af344a21f53bb84486ae2fb4bb4dfc1
           <Fragment key={index}>
             <svg className={`sprite__star_${prop.text}`} fill={fill}>
               <use xlinkHref="#star" />
