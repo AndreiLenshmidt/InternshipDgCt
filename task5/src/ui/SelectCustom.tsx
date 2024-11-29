785cb98e1ea2b608a115a6bf5855624c54a9d77f
SeddiMK <web.egorovm@gmail.com>
2024-11-29 19:33:47 +0300

    feat:  начало модалка редактировать задачу. модалка "Закрыть окно?". castom select.

---
+++ task5/src/ui/SelectCustom.tsx
@@ -0,0 +1,84 @@
+import React, { useState, useEffect } from 'react';
+import style from '@/styles/SelectCustom.module.scss';
+
+type SelectCustomProps<T> = {
+  value: T;
+  onChange: (value: T) => void;
+  options: T[]; // Опции для выпадающего списка
+  titleSelect: string; // Подсказка при выборе
+  label?: string; // Текст для `<label>`
+  required?: boolean; // Указание, обязательное поле или нет
+  errors?: string; // Сообщение об ошибке
+  isLoading?: boolean; // Флаг загрузки
+  fetchError?: string; // Сообщение об ошибке при загрузке
+  optionRenderer?: (option: T) => React.ReactNode; // Позволяет кастомизировать отображение опций
+};
+
+export default function SelectCustom<T>({
+  value,
+  onChange,
+  options,
+  titleSelect,
+  label,
+  required,
+  errors,
+  isLoading,
+  fetchError,
+  optionRenderer,
+}: SelectCustomProps<T>) {
+  const [taskTypes, setTaskTypes] = useState<string[]>([]); //<{ label: string; value: string }[]>
+  const [loading, setLoading] = useState<boolean>(true);
+  // const [fetchError, setFetchError] = useState<string>('');
+
+  useEffect(() => {
+    setTaskTypes([
+      'Задача',
+      'Баг',
+      'Улучшение',
+      'Новая функциональность',
+      'Эпик',
+      'Релиз',
+      'Бэклог',
+    ]);
+  }, []);
+
+  // console.log(taskTypes, 'taskTypes');
+  console.log(options, 'options');
+
+  return (
+    <div className={style['select-custom']}>
+      {label && (
+        <label htmlFor="select-custom">
+          {label} {required && <span>*</span>}
+        </label>
+      )}
+      {isLoading ? (
+        <p>Загрузка...</p> // Показываем индикатор загрузки
+      ) : fetchError ? (
+        <p className={style.error}>{fetchError}</p> // Если ошибка, показываем сообщение
+      ) : (
+        <select
+          className={style.select}
+          id="select-custom"
+          value={value as unknown as string} // Преобразуем тип для использования в HTML
+          onChange={(e) => onChange(e.target.value as T)}
+          required={required}
+        >
+          <option value="" disabled>
+            {titleSelect}
+          </option>
+          {options.map((option, index) => (
+            <option
+              className={style.options}
+              key={index}
+              value={option as unknown as string} // Преобразуем тип для значения
+            >
+              {optionRenderer ? optionRenderer(option) : String(option)}
+            </option>
+          ))}
+        </select>
+      )}
+      {errors && <p className={style.error}>{errors}</p>}
+    </div>
+  );
+}
--- task5/tailwind.config.js
+++ task5/tailwind.config.js
@@ -1,9 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+	content: [
+		// убрать лишнее
+		'./pages/**/*.{js,ts,jsx,tsx}',
+		'./components/**/*.{js,ts,jsx,tsx}',
+		'./ui/**/*.{js,ts,jsx,tsx}',
+		'./app/**/*.{js,ts,jsx,tsx}',
+		'./src/**/*.{js,ts,jsx,tsx}',
+	],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
 }
-
