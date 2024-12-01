f21277a848904e930be90e28966a382466aa81d4
Alexandr <digital-mag@ya.ru>
2024-12-01 14:29:45 +0300

    feat: небольшая адаптация фильтров для ширины планшета

--- task5/src/modules/KanbanPage/KanbanPage.tsx
+++ task5/src/modules/KanbanPage/KanbanPage.tsx
@@ -4,11 +4,9 @@ import { useRouter } from 'next/router';
 import style from './kanban-page.module.css';
 import { Switch } from '../../components/switch/Switch';
 
-
 const projectUrl = 'projects';
 
 export function KanbanPage() {
-
    const router = useRouter();
 
    console.log(router.query['task-slug']);
@@ -24,11 +22,43 @@ export function KanbanPage() {
          />
 
          <div className={style.title}>
-
             <h1>[Demo Project]</h1>
-            
+
             <Switch onChange={(v) => v} checked={false} />
             <h6>Только мои</h6>
+
+            <button>
+               <span>+</span>
+               Добавить задачу
+            </button>
+         </div>
+
+         <div className={style.filters}>
+            <div>
+               <label htmlFor="task_name">Название задачи</label>
+               <input name="task_name" type="text" placeholder="Название задачи" />
+            </div>
+            <div>
+               <label htmlFor="username">Выбрать пользователей</label>
+               <input name="username" type="text" placeholder="Пользователи" />
+            </div>
+            <div>
+               <label htmlFor="username">Выбрать тип</label>
+               <input name="username" type="text" placeholder="Выбрать тип" />
+            </div>
+            <div>
+               <label htmlFor="username">Выбрать компонент</label>
+               <input name="username" type="text" placeholder="Выбрать компонент" />
+            </div>
+         </div>
+
+         <div className={style.filters}>
+            <div>
+               <input name="username" type="text" placeholder="Дата начала" />
+            </div>
+            <div>
+               <input name="username" type="text" placeholder="Дата завершения" />
+            </div>
          </div>
       </>
    );
