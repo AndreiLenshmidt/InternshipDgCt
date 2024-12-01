e69bfc33bcb2c5a7c5954363350122f4e42818c7
Alexandr <digital-mag@ya.ru>
2024-12-01 11:55:29 +0300

    fixed: ширина меню ломалась при смене страниц

--- task5/src/modules/ProgectsPage/components/project_card/ProjectCard.tsx
+++ task5/src/modules/ProgectsPage/components/project_card/ProjectCard.tsx
@@ -1,18 +1,20 @@
-import style from './project-card.module.css';
 import FavLogo from '@public/images/icons/favorite-icon.svg';
+import Link from 'next/link';
+import style from './project-card.module.css';
 
 // import ProjectLogo1 from '@public/images/media/ProjectLogo.svg';
 
+type PropsType = {};
 
-export function ProjectCard() {
+export function ProjectCard({}: PropsType) {
    return (
-      <div className={style.card}>
+      <Link className={style.card} href={'/projects/44'}>
          <div className={style.favorite_icon}>
             <FavLogo />
          </div>
          <img src="/images/media/ProjectLogo.svg" alt="." />
          <h5>DS Внутренние проекты</h5>
          <div className={style.hint}>12 сотрудников</div>
-      </div>
+      </Link>
    );
 }
