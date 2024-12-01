9017f0ce5730f6744a259559e997374f9a29bd74
Alexandr <digital-mag@ya.ru>
2024-12-01 23:28:12 +0300

    fixed: путь к стилям в ModalClose

--- task5/src/ui/ModalClose/index.tsx
+++ task5/src/ui/ModalClose/index.tsx
@@ -1,7 +1,8 @@
 import React, { useEffect } from 'react';
 import Image from 'next/image';
 import Close from '@public/icons/close.svg';
-import styles from '@/styles/ModalClose.module.scss';
+import styles from '@/ui/ModalClose/ModalClose.module.scss';
+
 
 type ModalProps = {
    title: string;
