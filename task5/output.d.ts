ad9d2545a17a806f7bff3f58fc493eb8a301b850
Alexandr <digital-mag@ya.ru>
2024-12-02 22:22:50 +0300

    feat/todo проблема с получением данных (401 ошибка)

--- task5/output.d.ts
+++
@@ -1,3 +0,0 @@
-Need to install the following packages:
-sw2dts@2.6.3
-Ok to proceed? (y) Terminate batch job (Y/N)? Terminate batch job (Y/N)? Terminate batch job (Y/N)? Terminate batch job (Y/N)? Terminate batch job (Y/N)? Terminate batch job (Y/N)? Terminate batch job (Y/N)? Terminate batch job (Y/N)? Terminate batch job (Y/N)? 
