255f151b0872f71b115174e7d85b8f1ab46f70ec
Alexandr <digital-mag@ya.ru>
2024-12-02 21:10:01 +0300

    feat: сгенерировны типы для апи

---
+++ task5/output.d.ts
@@ -0,0 +1,3 @@
+Need to install the following packages:
+sw2dts@2.6.3
+Ok to proceed? (y) Terminate batch job (Y/N)? Terminate batch job (Y/N)? Terminate batch job (Y/N)? Terminate batch job (Y/N)? Terminate batch job (Y/N)? Terminate batch job (Y/N)? Terminate batch job (Y/N)? Terminate batch job (Y/N)? Terminate batch job (Y/N)? 
