3f5d2c921c1dab74030e5bbffc9c5d813efff1ad
Alexandr <digital-mag@ya.ru>
2024-12-10 23:08:59 +0300

    feat: определен drop метод

--- task5/src/modules/KanbanPage/components/tasks-column/TaskColumn.tsx
+++ task5/src/modules/KanbanPage/components/tasks-column/TaskColumn.tsx
@@ -1,6 +1,6 @@
 import { Stage } from '@/api/data.types';
 // import { useDroppable } from '@dnd-kit/core';
-import { LegacyRef, PropsWithChildren } from 'react';
+import { LegacyRef, PropsWithChildren, useState } from 'react';
 import { useDrop } from 'react-dnd';
 import style from '../../kanban-page.module.css';
 
@@ -10,12 +10,21 @@ export function TasksColumn({ children, stage, tasksAmount }: { stage: Stage; ta
    //    id: stage.id || 'droppable',
    // });
 
-   // const dropstyle = {
-   //    backgroundColor: isOver ? 'lightgray' : undefined,
-   // };
+   const [isOver, setOver] = useState(false);
+
+   const dropstyle = {
+      backgroundColor: isOver ? 'lightgray' : 'red',
+   };
 
   const [collectedProps, drop] = useDrop(() => ({
      accept: 'text',
+     hover(item, monitor) {
+      //   console.log(item, stage.name, monitor.isOver());
+      //   setOver(monitor.isOver());
+     },
+     drop(item, monitor) {
+        console.warn(item);
+     }
   }));   
 
    return (
@@ -23,7 +32,9 @@ export function TasksColumn({ children, stage, tasksAmount }: { stage: Stage; ta
       <div className="col" ref={drop as unknown as LegacyRef<HTMLDivElement>}>
          <h4 data-count={tasksAmount}>{stage.name}</h4>
          {/* style={dropstyle} */}
-         <div className={style.tasks}>{children}</div>
+         <div className={style.tasks}>
+            {children}
+         </div>
       </div>
    );
 }
