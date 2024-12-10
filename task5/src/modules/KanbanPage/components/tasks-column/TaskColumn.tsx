ba5246eb49e305da7847bc3ff3f3bf3e2b672b9d
Alexandr <digital-mag@ya.ru>
2024-12-10 22:24:43 +0300

    fix: подчистил за dnd-kit

--- task5/src/modules/KanbanPage/components/tasks-column/TaskColumn.tsx
+++ task5/src/modules/KanbanPage/components/tasks-column/TaskColumn.tsx
@@ -1,24 +1,23 @@
 import { Stage } from '@/api/data.types';
-import { useDroppable } from '@dnd-kit/core';
 import { PropsWithChildren } from 'react';
 import style from '../../kanban-page.module.css';
 
 export function TasksColumn({ children, stage, tasksAmount }: { stage: Stage; tasksAmount: number } & PropsWithChildren) {
    //
-   const { isOver, setNodeRef } = useDroppable({
-      id: stage.id || 'droppable',
-   });
+   // const { isOver, setNodeRef } = useDroppable({
+   //    id: stage.id || 'droppable',
+   // });
 
-   const dropstyle = {
-      backgroundColor: isOver ? 'lightgray' : undefined,
-   };
+   // const dropstyle = {
+   //    backgroundColor: isOver ? 'lightgray' : undefined,
+   // };
 
    return (
-      <div className="col" ref={setNodeRef}>
+      // ref={setNodeRef}
+      <div className="col">
          <h4 data-count={tasksAmount}>{stage.name}</h4>
-         <div className={style.tasks} style={dropstyle}>
-            {children}
-         </div>
+         {/*  style={dropstyle} */}
+         <div className={style.tasks}>{children}</div>
       </div>
    );
 }
