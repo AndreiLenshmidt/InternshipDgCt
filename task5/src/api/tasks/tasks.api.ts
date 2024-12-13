40d4c8792f70098f9a7e4d743e923bae54042902
Alexandr <digital-mag@ya.ru>
2024-12-13 21:45:50 +0300

    feat: попытка сделать обновление общего стэйта через invalidatesTags

--- task5/src/api/tasks/tasks.api.ts
+++ task5/src/api/tasks/tasks.api.ts
@@ -8,13 +8,14 @@ import { TypeRootState } from '@/store/store';
 
 
 export const tasksApi = createApi({
+   tagTypes: ['AllTasks', 'Task'],
    reducerPath: 'api/tasks',
    baseQuery: fetchBaseQuery({ baseUrl: BASE_API_URL, prepareHeaders }),
    endpoints: (build) => ({
       getTaskTypes: build.query<{ data: Array<TaskType> }, void>({ query: () => `/task_type` }),
       getTaskTags: build.query<{ data: Array<Component & {color?: string}> }, void>({ query: () => `/component` }),
       getTaskPriorities: build.query<{ data: Array<Priority> }, void>({ query: () => `/priority` }),
-      getAllTasks: build.query<{ data: Array<TaskMultiple> }, string>({ query: (slug: string) => `/project/${slug}/task` }),
+      getAllTasks: build.query<{ data: Array<TaskMultiple> }, string>({ query: (slug: string) => `/project/${slug}/task`, providesTags: ['Task'], }),
 
       updateTask: build.mutation<TaskMultiple, Partial<TaskMultiple> & Pick<TaskMultiple, 'id'>>({
          query: (task) => {
@@ -27,6 +28,7 @@ export const tasksApi = createApi({
                body: patch,
             }
          },
+         invalidatesTags: ['AllTasks']
          // transformResponse: (response: { data: TaskMultiple }, meta, arg) => response.data,
       })
       // getTaskStages: build.query<{ data: Array<Stage> }, void>({ query: () => `/stage` }),
