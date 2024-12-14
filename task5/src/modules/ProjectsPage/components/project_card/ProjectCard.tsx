b70e5bca37b979efc82c27e92d4a57ab03aefe7f
Alexandr <digital-mag@ya.ru>
2024-12-14 15:31:54 +0300

    fix: обозначение цветом значка избранных проектов

--- task5/src/modules/ProjectsPage/components/project_card/ProjectCard.tsx
+++ task5/src/modules/ProjectsPage/components/project_card/ProjectCard.tsx
@@ -5,12 +5,11 @@ import { useState } from 'react';
 import { ProjectItem, useUpdateProjectMutation } from '../../api/api';
 import style from './project-card.module.css';
 
-
 // import ProjectLogo1 from '@public/media/ProjectLogo.svg';
 
 type PropsType = {
    project?: ProjectItem;
-   onChange?: (isFavorite: boolean) => void
+   onChange?: (isFavorite: boolean) => void;
 };
 
 export function ProjectCard({ project, onChange }: PropsType) {
@@ -22,30 +21,23 @@ export function ProjectCard({ project, onChange }: PropsType) {
    });
 
    /**
-    * 
-    * @param e 
+    *
+    * @param e
     */
    const switchFavoriteState = (e: React.MouseEvent<HTMLDivElement>) => {
       e.preventDefault();
 
-      // const markFavorite = !isFavorite;
-
-      if (project) {
-                  
-         // setFavorite(markFavorite);
-         // if (onChange) {
-         //    onChange(markFavorite);
-         // }
-   
-         updateProject({id: project?.id as number, type: 'project', setFavorite: !project.is_favorite})
-      }
-
+      updateProject({ id: project?.id as number, type: 'project', setFavorite: !project?.is_favorite });
    };
 
    return (
       <Link className={style.card} href={'/projects/' + project?.slug}>
-         <div className={style.favorite_icon} onClick={switchFavoriteState}>
-            <FavLogo />
+         <div
+            className={style.favorite_icon}
+            style={project?.is_favorite ? { backgroundColor: `var(--main-blue)` } : {}}
+            onClick={switchFavoriteState}
+         >
+            {!project?.is_favorite ? <FavLogo /> : ''}
          </div>
          <img src={project?.logo?.link ? BASE_URL + project?.logo?.link : '/media/ProjectLogo.svg'} alt="." />
          <h5>{project?.name || 'DS Внутренние проекты'}</h5>
